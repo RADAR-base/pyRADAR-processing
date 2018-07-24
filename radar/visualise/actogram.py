8372f318893e88b59b4822b2c2e9108e0f785385
Callum Stewart <callum.stewart@kcl.ac.uk>
2018-07-24 13:50:45 +0000

    daily actogram

--- radar/visualise/actogram.py
+++ radar/visualise/actogram.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 week = pd.Timedelta('7d')
-def plot(dataframe, datacol='value.x', **kwargs):
+def weekly_plot(dataframe, datacol='value.x', **kwargs):
     groups = dataframe.groupby([dataframe.index.year, # dataframe.index.month,
                                 dataframe.index.week])
     n = len(groups)
@@ -16,9 +16,26 @@ def plot(dataframe, datacol='value.x', **kwargs):
         ax.set_xlim(start, end)
         if ax.legend_:
             ax.legend_.remove()
-    # mticks = axes[n-1].get_xaxis().get_majorticklabels()
-    # mticks[1].set_text('00:00')
-    # mticks[2].set_text('00:00')
     plt.subplots_adjust(hspace=0.5)
+    fig.tight_layout()
+    return fig
+
+day = pd.Timedelta('1d')
+def daily_plot(dataframe, datacol, **kwargs):
+    groups = dataframe.groupby([dataframe.index.year, dataframe.index.month,
+                                dataframe.index.day])
+    n = len(groups)
+    fig, axes = plt.subplots(n, 1, sharey=True, figsize=(8, 2*n))
+    for i, group in enumerate(groups):
+        ax = axes[i] if str(type(axes)) == "<class 'numpy.ndarray'>" else axes
+        start = pd.Timestamp(group[1].index[0].date())
+        end = start + day
+        group[1].plot(y=datacol, ax=ax, **kwargs)
+        ax.set_xlim(start, end)
+        if hasattr(ax, 'legend_') and ax.legend_:
+            ax.legend_.remove()
+    plt.subplots_adjust(hspace=0)
+    plt.tick_params(axis='both', which='both', bottom=False, top=False,
+            labelbottom=False, right=False, left=False, labelleft=False)
     fig.tight_layout()
     return fig
