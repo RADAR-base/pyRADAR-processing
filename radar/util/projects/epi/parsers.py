c622818ea1dc86b4af0282ab2e6d62c1d9240d60
Callum Stewart <callum.stewart@kcl.ac.uk>
2018-07-30 12:46:33 +0000

    rename util redcap to parsers. adds comments

--- radar/util/projects/epi/parsers.py
+++ radar/util/projects/epi/parsers.py
@@ -3,17 +3,35 @@ import pandas as pd
 import numpy as np
 
 class RedcapCsv():
+    """ A class to parse information from an EPI Redcap CSV extract.
+        The data dictionary can be found on the radar-base github.
+    """
     def __init__(self, csv_path, timezone):
+        """
+        Parameters
+        __________
+        csv_path : str
+            path of the csv file
+        timezone : str / tz representation
+            The timezone that the CSV has times recorded in.
+        """
         self.df = pd.read_csv(csv_path)
         self.tz = timezone
-    def seizure_times(self):
-        def make_datetime(date, time):
-            return pd.to_datetime(date + 'T' + time)
 
+    def seizure_times(self):
+        """
+        Returns
+        _______
+        labels : dict
+            A dict where each key is a patient code with another dict as
+            a value. That dict has keys representing labels ('Seizure') and
+            values of a dataframe with start and end times.
+        """
         df = self.df.copy()
         cols = ('clin_start', 'clin_end', 'eeg_start', 'eeg_end')
         for c in cols:
-            df[c] = make_datetime(df.rep_seizure_date, df['rep_seizure_' + c])
+            df[c] = pd.to_datetime(df.rep_seizure_date + 'T' +
+                                   df['rep_seizure_' + c])
         df['start'] = df[['clin_start', 'eeg_start']].min(axis=1)
         df['end'] = df[['clin_end', 'eeg_end']].min(axis=1)
         df.start = df.start.dt.tz_localize(self.tz)
@@ -27,7 +45,6 @@ class RedcapCsv():
             labels[ptc_code]['Seizure'] = \
                     seizures[['start', 'end']].reset_index(drop=True)
         return labels
-
 
     def ptc_info(self):
         pass
