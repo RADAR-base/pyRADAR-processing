eb49deb7bbdc2c97c80a80e2945017d8f55f9fdf
Callum Stewart <callum.stewart@kcl.ac.uk>
2019-02-14 16:06:05 +0000

    Function comments, errs

--- radar/common.py
+++ radar/common.py
@@ -1,15 +1,29 @@
+""" Commonly used functions the log -
+This should probably be merged with generic.py, and the
+log moved to it's own file
+"""
 import os
 import logging
-import numpy as np
 from datetime import datetime
+
+import numpy as np
+
 from . import config
 
-def abs_path(path):
+
+def abs_path(path: str):
+    """ Returns an absolute path if the path is not already absolute
+    Args:
+        path (str): Relative or absolute file path
+    Returns:
+        str: absolute path
+    """
     if not (os.path.isabs(path) or len(path.split('://')) > 1):
         path = os.path.abspath(path)
-    elif path[-1] is '/':
+    elif path[-1] == '/':
         path = path[:-1]
     return path
+
 
 def obj_col_names(obj):
     """ Returns a list of column names from a numpy record array or pandas
@@ -17,27 +31,35 @@ def obj_col_names(obj):
     """
     if isinstance(obj, np.ndarray):
         return list(obj.dtype.names)
-    else:
-        return list(obj.columns)
+    return list(obj.columns)
 
-def add_cli_log(log, formatter, level=logging.ERROR):
+
+def add_cli_log(logger: logging.Logger,
+                formatter: logging.Formatter,
+                level: int = logging.ERROR) -> None:
+    """ Creates CLI log of RADAR errors and above, used by default
+    """
     clilog = logging.StreamHandler()
     clilog.setLevel(level)
     clilog.setFormatter(formatter)
-    log.addHandler(clilog)
-    return log
+    logger.addHandler(clilog)
 
-def add_file_log(log, formatter, level=logging.WARNING):
-    filename = 'radar_{:%Y%m%d-%H%M}.log'.format(datetime.now()) 
+
+def add_file_log(logger: logging.Logger,
+                 formatter: logging.Formatter,
+                 level: int = logging.WARNING) -> None:
+    """ A file-based logger for warnings, debug, etc.
+    Requires log_to_file config flag == True """
+    filename = 'radar_{:%Y%m%d-%H%M}.log'.format(datetime.now())
     filelog = logging.FileHandler(filename)
     filelog.setLevel(level)
     filelog.setFormatter(formatter)
-    log.addHandler(filelog)
-    return log
+    logger.addHandler(filelog)
+
 
 log = logging.getLogger('radarlog')
-formatter = logging.Formatter('%(asctime)s - %(name)s' + \
-                              ' - %(levelname)s - %(message)s')
-log = add_cli_log(log, formatter)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+add_cli_log(log, formatter)
 if config['log_to_file']:
-    log = add_file_log(log, formatter)
+    add_file_log(log, formatter)
--- radar/io/sql.py
+++ radar/io/sql.py
@@ -1,13 +1,17 @@
 """ IO operations for SQL databases - currently Postgres specific
 """
 import time
-from typing import Callable, Any
+from typing import Callable, Union, Any
 from io import StringIO
 from functools import wraps
 from itertools import chain
+
+import pandas as pd
 import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import connection as Connection
+
+from ..common import log
 
 
 def try_sql(func: Callable[[Connection, Any], Any]) -> Callable:
@@ -15,20 +19,30 @@ def try_sql(func: Callable[[Connection, Any], Any]) -> Callable:
     Cancels the connection on error, commits on success.
     """
     @wraps(func)
-    def wrapper(conn, *args, **kwargs):
+    def wrapper(conn: Connection, *args, **kwargs) -> Union[Any, str]:
+        """ Try-except wrapper. Returns func output on success"""
         try:
             out = func(conn, *args, **kwargs)
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-            out = error
+        except psycopg2.Error as err:
+            log.error('SQL error with func %s, args %s', func, args)
             conn.cancel()
+            raise err
         conn.commit()
         return out
     return wrapper
 
 
 @try_sql
-def df_to_psql(conn, table, df):
+def df_to_psql(conn: Connection, table: str, df: pd.DataFrame) -> None:
+    """ Insert a pandas DataFrame into a Postgres table using a
+    StringIO intermediary CSV to COPY FRO
+    Args:
+        conn: psycopg2 connection
+        table (str): postgres table name
+        df (pandas.DataFrame): dataframe to insert
+    Returns:
+        None
+    """
     cur = conn.cursor()
     f = StringIO()
     df.to_csv(f)
@@ -59,7 +73,12 @@ def create_table_like(conn: Connection,
 
 
 @try_sql
-def drop_table(conn, table):
+def drop_table(conn: Connection, table: str):
+    """ Drop a table (dynamic name)
+    Args:
+        conn: psycopg2 connection
+        table (str): table name
+    """
     cur = conn.cursor()
     cmd = sql.SQL('DROP TABLE {}').format(sql.Identifier(table))
     cur.execute(cmd)
@@ -93,7 +112,14 @@ def upsert_from_table(conn, source_table, target_table, columns):
     cur.execute(cmd)
 
 
-def df_upsert_psql(conn, table, df):
+def df_upsert_psql(conn: Connection, table: str, df: pd.DataFrame) -> None:
+    """ Upsert a Dataframe into a Postgres table through:
+    DataFrame -> StringIO -> Temp table -> table
+    Args:
+        conn: psycopg2 connection
+        table (str): Table to upsert data into
+        df (pd.DataFrame): Dataframe containing data to upsert from
+    """
     columns = [df.index.name] + list(df.columns)
     curr_time = str(time.time()).replace('.', '_')
     tmp_table = 'staging_' + table + '_' + curr_time
