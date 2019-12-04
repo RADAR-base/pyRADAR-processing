15023c7e43f23590045927ca545836ac6044a954
Callum Stewart <callum.stewart@kcl.ac.uk>
2019-12-04 18:02:44 +0000

    project and participant classes

--- radar/generic.py
+++ radar/generic.py
@@ -68,50 +68,6 @@ class RecursiveDict(dict):
         return len(self._get_keys())
 
 
-class AttrRecDict(RecursiveDict):
-    """ A dictionary, based on RecursiveDict, that allows recurisve access to
-    nested dictionary items using object attributes.
-    Only the final level / leaf dictionaries will have attribute accessible
-    items. Primarily used to access subproject participants from a higher level
-    project.
-    Example:
-        d = AttrRecDict((('inner', {'innerkey':'innerval'}),))
-        d.inner raises an AttributeError
-        d.innerkey returns 'innerval'
-        d['inner'] returns {'inner': 'innerkey'}
-        d['inner/innerkey'] returns 'innerval
-    """
-    def __getattr__(self, name):
-        if name not in [x for x in self._get_keys()]:
-            raise AttributeError(
-                "No such attribute '{}' in '{}'".format(name, self))
-        kv = self._get_items()
-        val = [x[1] for x in kv if x[0] == name] or None
-        if val is None:
-            raise AttributeError(
-                "No such attribute '{}' in '{}'".format(name, self))
-        elif len(val) > 1:
-            raise ValueError(
-                'Multiple participants with the same ID: {}'.format(name))
-        return val[0]
-
-    def __repr__(self):
-        string = super(AttrRecDict, self).__repr__()
-        string = string.split('\n')
-        string[0] = 'Attribute recursive dict with'
-        string = '\n'.join(string)
-        return string
-
-    def __dir__(self):
-        return self.keys()
-
-
-class AttrDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(AttrDict, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-
-
 def update(d, u):
     for k, v in u.items():
         if isinstance(v, collections.Mapping):
@@ -128,6 +84,7 @@ def methdispatch(func):
     wrapper.register = dispatcher.register
     update_wrapper(wrapper, func)
     return wrapper
+
 
 @lru_cache(8)
 def re_compile(pattern):
--- radar/wrappers.py
+++ radar/wrappers.py
@@ -1,105 +1,112 @@
 #!/usr/bin/env python3
 import os
+from collections.abc import MutableMapping, KeysView, ItemsView, ValuesView
 import pandas as pd
+from fsspec import filesystem
+from fsspec.utils import infer_storage_options
 from . import config
-from .generic import AttrRecDict, update
+from .generic import update
 from .common import abs_path, log
 from .io.loaders import search_project_dir, search_dir_for_data, load_data_path
 
 
-
-class PtcDict(AttrRecDict):
-    """ Dictionary to contain participants
-    It is an attribute / recursive dictionary
-    (values can be accessed through __getattribute__,
-     and sub-dictionaries can be accessed by splitting the
-     key with "/")
-    """
-    def __repr__(self):
-        return 'Participants:\n\t' + '\n\t'.join(self._get_keys())
-
-
-class RadarObject():
-    """ Base class for RADAR Project, Participant, and Data classes
-    Provides methods for accessing parent class attributes, although
-    several are semi-depricated
-    """
-    _parent = None
-
+class RadarObject(MutableMapping):
+    _keys_view = KeysView
+    _items_view = ItemsView
+    _values_view = ValuesView
+    def __init__(self, path, populate=False, parent=None, name=None, **kwargs):
+        self.parent = parent
+        self._subobject_class = RadarObject
+        specs = infer_storage_options(path)
+        self.path = specs.pop('path')
+        self.fs = filesystem(**specs)
+        self.path = self.fs.info(self.path)['name']
+        self.name = name if name else self.path.split(self.fs.sep)[-1]
+        self.store = dict()
+        if populate:
+            self._populate_store(populate=True)
+        self._populated = populate
+    def _list_subobjects(self):
+        return self.fs.listdir(self.path)
+    def _populate_store(self, populate=False):
+        paths = self._list_subobjects()
+        for p in paths:
+            if p['type'] == 'directory':
+                basename = p['name'].split(self.fs.sep)[-1]
+                self.store[basename] = self._subobject_class(p['name'], parent=self)
+        self._populated = True
+    def __setitem__(self, key, val):
+        self.store[key] = val
+    def __getitem__(self, key):
+        if not self._populated:
+            self._populate_store()
+        split_key = key.split('/')
+        if len(split_key) == 1:
+            return self.store[key]
+        else:
+            base_key = split_key[0]
+            sub_key = '/'.join(split_key[1:])
+            return self.store[base_key][sub_key]
+    def __delitem__(self, key):
+        del self.store[key]
+    def __iter__(self):
+        return iter(self.store)
+    def __len__(self):
+        return len(self.store)
+    def keys(self):
+        if not self._populated:
+            self._populate_store()
+        return self._keys_view(self)
+    def items(self):
+        if not self._populated:
+            self._populate_store()
+        return self._items_view(self)
+    def values(self):
+        if not self._populated:
+            self._populate_store()
+        return self._values_view(self)
+    def _ipython_key_completions_(self):
+        return list(self.keys())
     def _get_attr_or_parents(self, attr):
         if hasattr(self, attr) and getattr(self, attr) is not None:
             res = getattr(self, attr)
-        elif getattr(self, '_parent') is None:
+        elif getattr(self, 'parent') is None:
             res = None
         else:
-            res = self._parent._get_attr_or_parents(attr)
-
+            res = self.parent._get_attr_or_parents(attr)
         return res
-
-    def _get_path(self):
-        """ Returns a path to the RadarObject relative to the project root,
-        based on subproject names
-
-        Output
-        ______
-        path: str
-            A string of the relative path.
-        """
-        parent = self._parent
-        if parent is None:
-            return os.path.sep
-        path = os.path.sep + (self.name if hasattr(self, 'name') else '')
-        while parent._parent is not None:
-            path = os.path.sep + parent.name + path
-            parent = parent._parent
-        return path
-
-    @staticmethod
-    def _norm_paths(paths):
-        paths = [paths] if isinstance(paths, str) else \
-                paths if isinstance(paths, list) else []
-        return [abs_path(p) for p in paths]
-
-    def _p_schemas(self):
-        return self._get_attr_or_parents('schemas')
-
-    def _p_specifications(self):
-        return self._get_attr_or_parents('specifications')
-
-    def _p_armt_definitions(self):
-        return self._get_attr_or_parents('armt_definitions')
-
-    def _p_armt_protocols(self):
-        return self._get_attr_or_parents('armt_protocols')
+    def __str__(self):
+        return (self.__class__.__module__ + '.' +
+                self.__class__.__name__ + ': "' + self.name + '"')
+    @property
+    def schemas(self):
+        return self._get_attr_or_parents('_schemas')
+    @property
+    def specifications(self):
+        return self._get_attr_or_parents('_specifications')
+    @property
+    def armt_definitions(self):
+        return self._get_attr_or_parents('_armt_definitions')
+    @property
+    def armt_protocols(self):
+        return self._get_attr_or_parents('_armt_protocols')
 
 
 class Project(RadarObject):
     """
     """
-    def __init__(self, path=None, name='', parent=None, **kwargs):
+    def __init__(self, path, **kwargs):
         """
         Parameters
         __________
-        name : str
-            The name of the project
         path : list / str
             Path(s) containing project data
-        parent : radar.Project / None
-            A radar.Project instance if the project is a subproject, else None
-        subprojects : list
-            A list of subprojects to load in paths
+        name : str
+            The name of the project
         participants : list
             A list of participants to load in paths
         blacklist : list
             Files/folders to ignore in paths
-        ptckw : dict
-            Participant keywords
-        datakw : dict
-            Keywords for participant's data loading
-        info : dict
-            dict of dicts (key:participant id, inner dict: participant info)
-        labels : dict
-            dict of dicts (key: participant id, inner dict: participant labels)
         schemas : None
             --
         specifications : None
@@ -109,292 +116,82 @@ class Project(RadarObject):
         armt_protocols : None
             --
         """
-        self.name = name
-        self._parent = parent
-        self._paths = []
-        """
-        self.schemas = schemas
-        self.specifications = specifications
-        self.armt_definitions = armt_definitions
-        self.armt_protocols = armt_protocols
-        """
-        self.subprojects = AttrRecDict()
-        self.participants = self._parent.participants[self.name] if \
-            self._parent else PtcDict()
-        for p in self._norm_paths(path):
-            self.add_path(p, **kwargs)
+        super(Project, self).__init__(path, **kwargs)
+        self._keys_view = ProjectKeysView
+        self._items_view = ProjectItemsView
+        self._values_view = ProjectValuesView
+        self._subobject_class = Participant
+        self._schemas = kwargs.get('schemas')
+        self._specifications = kwargs.get('specifications')
+        self._armt_definitions = kwargs.get('armt_definitions')
+        self._armt_protocols = kwargs.get('armt_protocols')
+        self._whitelist = kwargs.get('participants')
+        self._blacklist = kwargs.get('blacklist', [])
+    def _list_subobjects(self):
+        all_paths = self.fs.listdir(self.path)
+        whitelist = [p['name'] for p in all_paths] if self._whitelist is None \
+            else self._whitelist
+        blacklist = self._blacklist
+        return [p for p in all_paths if
+                p['name'] in whitelist and
+                p['name'] not in blacklist]
+    @property
+    def participants(self):
+        return self.keys()
 
-        info = kwargs.get('info', False)
-        if info:
-            for ptc in info:
-                if ptc not in self.participants:
-                    self.add_participant(ptc)
-            self.ptcs_update_info(info)
-
-        labels = kwargs.get('labels', False)
-        if labels:
-            for ptc in labels:
-                if ptc not in self.participants:
-                    self.add_participant(ptc)
-                self.ptcs_update_labels(labels)
-
+class ProjectKeysView(KeysView):
     def __repr__(self):
-        return 'RADAR {} of type {}: {}, {} participants'\
-                .format('Subproject' if getattr(self, '_parent') is None
-                        else 'Project',
-                        type(self),
-                        self.name,
-                        len(self.participants))
+        keys_str = ', '.join(self._mapping.participants)
+        return 'ProjectParticipantKeys[' + keys_str + ']'
 
-    def __str__(self):
-        info_string = '''
-        member of {class}
-        Name: {name}
-        Subprojects: {subprojs}
-        No. participants: {num_partic}
-        '''
-        format_kwargs = {
-            'class': type(self),
-            'name': self.name,
-            'subprojs': ', '.join(self.subprojects.keys()) or 'None',
-            'num_partic': len(self.participants),
-        }
-        return info_string.format(**format_kwargs)
+class ProjectItemsView(ItemsView):
+    def __repr__(self):
+        items_str = ', '.join(['("{name}": {p})'.format(name=name,
+                                                        p=self._mapping[name])
+                               for name in self._mapping.participants])
+        return 'ProjectParticipantItems[' + items_str + ']'
 
-    def _ipython_key_completions_(self):
-        return list(self.participants.keys()) + list(self.subprojects.keys())
 
-    def __getitem__(self, key):
-        if key in self.subprojects:
-            return self.subprojects[key]
-        elif hasattr(self.participants, key):
-            return getattr(self.participants, key)
-        else:
-            raise KeyError('No such subproject or participant: {}'.format(key))
-
-    def add_participant(self, name, where='.', *args, **kwargs):
-        proj = self if where == '.' else self.subprojects[where]
-        if name in proj.participants:
-            log.debug('participant {} already exists in project {}'
-                      .format(name, proj.name))
-        else:
-            proj.participants[name] = Participant(name=name, parent=self,
-                                                  *args, **kwargs)
-        return proj.participants[name]
-
-    def add_subproject(self, name, where='.', *args, **kwargs):
-        proj = self if where == '.' else self.subprojects[where]
-        if name in proj.subprojects:
-            log.debug('Subproject {} already exists in project {}'
-                      .format(name, proj.name))
-        elif name in proj.participants:
-            log.error('The subproject name {} is already '.format(name) +
-                      'used as a participant in project {}'.format(proj.name))
-            return None
-        else:
-            proj.participants[name] = PtcDict()
-            proj.subprojects[name] = Project(name=name, parent=self,
-                                             *args, **kwargs)
-        return proj.subprojects[name]
-
-    def _parse_path(self, path, subprojects=None, participants=None,
-                    blacklist=None, **kwargs):
-        dir_dict = search_project_dir(path,
-                                      subprojects=subprojects,
-                                      participants=participants,
-                                      blacklist=blacklist)
-        return dir_dict
-
-    def _add_subprojects(self, paths, **kwargs):
-        for p in paths:
-            name = p.split(os.path.sep)[-1]
-            sp = self.add_subproject(name, **kwargs)
-            sp.add_path(p, **kwargs)
-
-    def _add_participants(self, paths, **kwargs):
-        for p in paths:
-            name = p.split(os.path.sep)[-1]
-            if name in self.participants:
-                ptc = self.participants[name]
-                ptc.add_path(p, **kwargs.get('datakw', {}))
-            else:
-                ptc = self.add_participant(name, paths=p, **kwargs)
-
-    def add_path(self, path, **kwargs):
-        self._paths.append(path)
-        dirs = self._parse_path(path, **kwargs)
-        self._add_subprojects(dirs['subprojects'], **kwargs)
-        ptckw = kwargs.get('ptckw', {})
-        ptckw.update(config.project.ptckw)
-        datakw = kwargs.get('datakw', {})
-        datakw.update(config.project.datakw)
-        self._add_participants(dirs['participants'], datakw=datakw, **ptckw)
-
-    def map_participants(self, func, *args, **kwargs):
-        def ptc_func(ptc, *args, **kwargs):
-            return func(ptc, *args, **kwargs)
-        return map(ptc_func, list(self.participants))
-
-    def _ptc_update_dict(self, new_dict, dictname):
-        for ptc in self.participants:
-            old_dict = getattr(ptc, dictname)
-            update(old_dict, new_dict.get(ptc.name, {}))
-
-    def ptcs_update_labels(self, labels):
-        self._ptc_update_dict(labels, 'labels')
-
-    def ptcs_update_info(self, info):
-        self._ptc_update_dict(info, 'info')
+class ProjectValuesView(KeysView):
+    def __repr__(self):
+        vals_str = ', '.join([str(self._mapping[name])
+                              for name in self._mapping.participants])
+        return 'ProjectParticipantValues[' + vals_str + ']'
 
 
 class Participant(RadarObject):
     """ A class to hold data and methods concerning participants/subjects in a
     RADAR trial. Typically intialised by opening a Project.
     """
-    def __init__(self, name=None, paths=None, **kwargs):
-        """
-        Parameters
-        __________
-        name : str
-
-        paths : str or list
-
-        info : dict (optional)
-
-        labels : dict (optional)
-
-        datakw : dict (optional)
-            Keywords for participant's data loading
-        """
-        if not (name or paths):
-            raise ValueError(('You must specify a name or else provide'
-                              'a path (or paths) to the participant'))
-        self.name = name if name is not None else paths[0].split(os.path.sep)[-1]
-        self._parent = kwargs.get('parent', None)
-        self.info = kwargs.get('info', {})
-        self.labels = kwargs.get('labels', {})
-        datakw = kwargs.get('datakw', {})
-        datakw.update(config.project.datakw)
-        self.data = ParticipantData(self, **datakw)
-        self._paths = []
-        for p in self._norm_paths(paths):
-            self.add_path(p, **datakw)
-
-    def __repr__(self):
-        return "Participant {}. of type {}".format(self.name, type(self))
-
-    def __getitem__(self, key):
-        return self.data[key]
-
-    def __setitem__(self, key, value):
-        self.data[key] = value
-
-    def _ipython_key_completions_(self):
-        return list(self.data.keys())
-
-    def add_path(self, path, **kwargs):
-        self._paths.append(path)
-        self.data._search_path(path, **kwargs)
-
-    def reparse_data(self, **datakw):
-        datakw.update(config.project.datakw)
-        self.data = ParticipantData(self, paths=self._paths, **datakw)
-
-    def export_data(self, data=None, names=None, project_path=None,
-                    filetype=None, *args, **kwargs):
-        raise NotImplementedError
-        """ Exports a data object to another file or format.
-        Parameters
-        __________
-        data : DataFrame / RecArray / RadarTable
-            Data object to save
-        name : str
-            Name under which to save the output
-        path : str
-        filetype : str
-        """
-        output = self._get_attr_or_parents('output')
-        given = (names, project_path, filetype)
-        generic = (self.name, output['project_path'], output['filetype'])
-        name, project_path, filetype = (
-            x if x is not None else (generic)[i]
-            for i, x in enumerate(given))
-
-        """
-        if not isinstance(output, ParticipantIO):
-            output = output.create_participant(where=self._parent._get_path(),
-                                               name=self.name)
-        output.create_table(name=name, obj=data, *args, **kwargs)
-        """
-
-
-class ParticipantData(RadarObject):
-
-    def __init__(self, participant, *args, **kwargs):
-        """
-        Parameters
-        ___________
-        participant : radar.Participant
-        data : dict
-            Dict storing data names as key and the data in a dask object as the
-            value. Strings are always assumed to be paths.
-        paths : str or list of str
-        """
-        self._parent = participant
-        self._data = kwargs.pop('data', {})
-        paths = self._norm_paths(kwargs.pop('paths', None))
-        for path in paths:
-            self._search_path(path, **kwargs)
-
-
-    def get(self, key, default=None):
-        if key in self:
-            return self.__getitem__(key)
-        return None
-
-    def __getitem__(self, key):
-        val = self._data[key]
-        if isinstance(val, str):
-            self._load(key, val)
-        return self._data[key]
-
-    def __setitem__(self, key, value):
-        self._data[key] = value
-
-    def __getattr__(self, key):
-        if key in self._data:
-            return self[key]
-        if hasattr(self._data, key):
-            return getattr(self._data, key)
-        raise AttributeError('No such attribute "{}" in {}'.format(key, self))
-
-    def __iter__(self):
-        return iter(self._data)
-
-    def __repr__(self):
-        topics = list(self._data.keys())
-        topics.sort()
-        return 'Participant data topics:\n {}'\
-            .format(', '.join(topics) if topics else 'None')
-
-    def __dir__(self):
+    def __init__(self, path, **kwargs):
+        super(Participant, self).__init__(path, **kwargs)
+        self._keys_view = ParticipantKeysView
+        self._items_view = ParticipantItemsView
+        self._values_view = ParticipantValuesView
+        self._subobject_class = RadarData
+    @property
+    def data(self):
         return self.keys()
 
-    def _ipython_key_completions_(self):
-        return list(self.keys())
+class ParticipantKeysView(KeysView):
+    def __repr__(self):
+        keys_str = ', '.join(self._mapping.data)
+        return 'ParticipantDataKeys[' + keys_str + ']'
 
-    def _search_path(self, path, replace=False, **kwargs):
-        modals = search_dir_for_data(path, **kwargs)
-        for k, v in modals.items():
-            if replace or (k not in self._data):
-                self._data[k] = modals[k]
+class ParticipantItemsView(ItemsView):
+    def __repr__(self):
+        items_str = ', '.join(['("{name}": {p})'.format(name=name,
+                                                        p=self._mapping[name])
+                               for name in self._mapping.data])
+        return 'ParticipantDataItems[' + items_str + ']'
 
-    def _load(self, name, path, **kwargs):
-        data = load_data_path(path, **kwargs)
-        if isinstance(data, dict):
-            self._data.update(data)
-        else:
-            self._data[name] = data
 
-    def save(self, outpath, outfmt, data_names=None, **kwargs):
-        raise NotImplementedError
+class ParticipantValuesView(KeysView):
+    def __repr__(self):
+        vals_str = ', '.join([str(self._mapping[name])
+                              for name in self._mapping.data])
+        return 'ParticipantDataValues[' + vals_str + ']'
+
+class RadarData():
+    def __init__(self, *args, **kwargs):
+        pass
